ae72803dd574507ed90b1dabe4b442a26ef8ba8c
LosHunterros <m.piech@piechdesign.pl>
2023-03-06 23:48:14 +0100

    Handling errors

--- ideabank.py
+++ ideabank.py
@@ -1,5 +1,6 @@
 from os import system, name
 from io import open
+from time import sleep
 
 def clear():
  
@@ -26,12 +27,17 @@ while new_idea.lower() != 'quit':
 
     print("\nType 'Quit' to finish")
 
+    idea_file = open(idea_file_name, mode="w", encoding="utf-8")
+    idea_file.write('\n'.join(idea_list))
+    idea_file.close()
+
     new_idea = input("\nWhat is your new idea? ")
 
     if new_idea[:8] == "--delete":
-        del idea_list[int(new_idea[9:])-1]
+        try:
+            del idea_list[int(new_idea[9:])-1]
+        except:
+            print("Wrong number")
+            sleep(2)
     else:
         idea_list.append(new_idea)
-        idea_file = open(idea_file_name, mode="w", encoding="utf-8")
-        idea_file.write('\n'.join(idea_list))
-        idea_file.close()
